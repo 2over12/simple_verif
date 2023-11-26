ab7035c7aeee38470aace6285f9abeb529f8a9f9
2over12 <ian.smith@trailofbits.com>
2023-11-26 13:29:55 -0500

    almost runnable

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,5 @@
 open Simple_verif
-module S = DPLL.SolverState (CCString)
+module S = DPLL.SolverState (UninterpretedFunctions)
 
 let () = Printexc.record_backtrace true;;
 
@@ -7,11 +7,11 @@ let tgt_args = Bos.OS.Arg.(parse ~pos:string ()) in
 let to_parse = List.hd tgt_args in
 let nnf_form =
   Bexp_parser.single_exp Bexp_lexer.read (Lexing.from_string to_parse)
-  |> Bexp.rewrite_to_nnf
+  |> EUF.rewrite_to_nnf
 in
-nnf_form |> Bexp.show_nnf_formula |> print_endline;
-let cnf_form = Bexp.rewrite_to_cnf_clauses nnf_form in
-cnf_form |> Bexp.show_cnf_formula |> print_endline;
+nnf_form |> EUF.show_nnf_formula |> print_endline;
+let cnf_form = EUF.rewrite_to_cnf_clauses nnf_form in
+cnf_form |> EUF.show_cnf_formula |> print_endline;
 match S.solve_inject cnf_form with
 | `Unsat -> print_endline "Unsat!"
 | `Sat st -> print_endline ("Sat! \n " ^ S.M.show st.model)
---
+++ lib/EUF.ml
@@ -0,0 +1,3 @@
+include Formula.Make (UninterpretedFunctions.L)
+module FuncExpr = UninterpretedFunctions.FuncExpr
+module EqCons = UninterpretedFunctions.EqCons
